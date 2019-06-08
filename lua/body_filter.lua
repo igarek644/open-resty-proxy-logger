ee743f308f4fb393ad49087140e48edf7ec18085
Ihor Pravdyvyi <igarek644@gmail.com>
2019-06-08 15:12:38 +0300

    Fix formatting

--- lua/body_filter.lua
+++ lua/body_filter.lua
@@ -1,5 +1,6 @@
  local response_body = string.sub(ngx.arg[1], 1, 1000)
-        ngx.ctx.buffered = (ngx.ctx.buffered or "") .. response_body
-        if ngx.arg[2] then
-        ngx.var.response_body = ngx.ctx.buffered
-        end
+ ngx.ctx.buffered = (ngx.ctx.buffered or "") .. response_body
+
+ if ngx.arg[2] then
+    ngx.var.response_body = ngx.ctx.buffered
+    end
