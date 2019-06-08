f4ba67fffc0144e520abcf937af544b3ddbfbf46
Ihor Pravdyvyi <igarek644@gmail.com>
2019-06-08 15:11:07 +0300

    Proxy logger

---
+++ lua/body_filter.lua
@@ -0,0 +1,5 @@
+ local response_body = string.sub(ngx.arg[1], 1, 1000)
+        ngx.ctx.buffered = (ngx.ctx.buffered or "") .. response_body
+        if ngx.arg[2] then
+        ngx.var.response_body = ngx.ctx.buffered
+        end
